ab5038b0598a1a42f75e0875c5be6b7c449c4fa3
David Elbakian <elbakiand@gmail.com>
2021-07-26 11:23:20 +0400

    javasyntaxpro9l3

---
+++ Main.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javasyntaxpro9l3;
+
+
+//import java.io.BufferedReader;
+//import java.io.StringReader;
+//import java.math.BigInteger;
+//import java.util.ArrayList;
+/**
+ *
+ * @author David-pc
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        /* 1
+        замени все короткие имена классов в коде на полные.
+        */
+        
+        java.util.ArrayList<java.lang.String> arrayList = new java.util.ArrayList<>();
+        java.io.BufferedReader bufferedReader = new java.io.BufferedReader(new java.io.StringReader(""));
+        java.math.BigInteger bigInteger = new java.math.BigInteger("123");
+    }
+    
+}
---
+++ Solution.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javasyntaxpro9l3;
+
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+/**
+ *
+ * @author David-pc
+ */
+public class Solution {
+    public static void main(String[] args) {
+        /* 2
+        В классе Solution замени все полные имена классов в коде на короткие.
+        */
+        
+        LinkedList<String> linkedList = new LinkedList<>();
+        InputStreamReader inputStreamReader = new InputStreamReader(System.in);
+        AtomicBoolean atomicBoolean = new AtomicBoolean(false);
+    }
+    
+}
